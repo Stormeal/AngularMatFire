217a16d6a951682714455d03c961c1fb9487bb56
Alex <stormeal@gmail.com>
2018-07-10 09:04:24 +0200

    initial commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,69 @@
+import { Routes } from '@angular/router';
+
+import { FullComponent } from './layouts/full/full.component';
+import { AppBlankComponent } from './layouts/blank/blank.component';
+
+export const AppRoutes: Routes = [
+  {
+    path: '',
+    component: AppBlankComponent,
+    children: [{
+      path: '',
+      redirectTo: '/authentication/login',
+      pathMatch: 'full'
+    }, {
+      path: '',
+      loadChildren: './authentication/authentication.module#AuthenticationModule'
+    }, {
+      path: 'home',
+      loadChildren: './dashboards/dashboard1'
+    }, {
+      path: '**',
+      redirectTo: 'authentication/404'
+    }],
+  }, {
+    path: '',
+    component: FullComponent,
+    children: [{
+      path: '',
+      redirectTo: '/dashboards/dashboard1',
+      pathMatch: 'full'
+    }, {
+      path: 'dashboards',
+      loadChildren: './dashboards/dashboards.module#DashboardsModule'
+    }, {
+      path: 'material',
+      loadChildren: './material-component/material.module#MaterialComponentsModule'
+    }, {
+      path: 'apps',
+      loadChildren: './apps/apps.module#AppsModule'
+    }, {
+      path: 'forms',
+      loadChildren: './forms/forms.module#FormModule'
+    }, {
+      path: 'tables',
+      loadChildren: './tables/tables.module#TablesModule'
+    }, {
+      path: 'datatables',
+      loadChildren: './datatables/datatables.module#DataTablesModule'
+    }, {
+      path: 'pages',
+      loadChildren: './pages/pages.module#PagesModule'
+    }, {
+      path: 'widgets',
+      loadChildren: './widgets/widgets.module#WidgetsModule'
+    }, {
+      path: 'charts',
+      loadChildren: './charts/chartslib.module#ChartslibModule'
+    }]
+  }, {
+    path: '',
+    component: AppBlankComponent,
+    children: [{
+      path: 'authentication',
+      loadChildren: './authentication/authentication.module#AuthenticationModule'
+    }]
+  }, {
+    path: '**',
+    redirectTo: 'authentication/404'
+  }];
